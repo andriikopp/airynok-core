1bf0e790e38d0e4aab2e61b5dcce0165452ae745
Andrii Kopp <kopp93@gmail.com>
2022-01-23 10:55:58 +0000

    Look for OG and Twitter metatags.

--- airynok.js
+++ airynok.js
@@ -1,67 +1,93 @@
 javascript: (() => {
 
-    var extractKeywords = (text, number) => {
+	var extractKeywords = (text, number) => {
 
-        var stopwords = [
-            'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now'
-        ];
+		var stopwords = [
+			'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now'
+		];
 
-        var words = text.split(' ');
+		var words = text.split(' ');
 
-        var keywords = [];
+		var keywords = [];
 
-        words.forEach(word => {
+		words.forEach(word => {
 
-            word = word.toLowerCase();
+			word = word.toLowerCase();
 
-            if (/^[a-z0-9]+$/i.test(word) && !stopwords.includes(word)) {
+			if (/^[a-z0-9]+$/i.test(word) && !stopwords.includes(word)) {
 
-                keywords.push(word);
-            }
-        });
+				keywords.push(word);
+			}
+		});
 
-        var keywordFrequency = {};
+		var keywordFrequency = {};
 
-        keywords.forEach(keyword => {
+		keywords.forEach(keyword => {
 
-            if (keywordFrequency[keyword] === undefined) {
+			if (keywordFrequency[keyword] === undefined) {
 
-                keywordFrequency[keyword] = 1;
+				keywordFrequency[keyword] = 1;
 
-            } else {
+			} else {
 
-                keywordFrequency[keyword]++;
-            }
-        });
+				keywordFrequency[keyword]++;
+			}
+		});
 
-        var final = [];
+		var final = [];
 
-        for (var keyword in keywordFrequency) {
+		for (var keyword in keywordFrequency) {
 
-            var weight = keywordFrequency[keyword] * Math.log(number / keywordFrequency[keyword]);
+			var weight = keywordFrequency[keyword] * Math.log(number / keywordFrequency[keyword]);
 
-            if (weight <= 0) {
+			if (weight <= 0) {
 
-                final.push(keyword);
-            }
-        }
+				final.push(keyword);
+			}
+		}
 
-        return final;
-    };
+		return final;
+	};
 
-    var pageTitle = document.title;
+	var pageTitle = document.title;
 
-    var pageMetaDescription = document
-        .querySelector('meta[name="description"]')
-        .content;
+	var pageMetaDescription = '';
 
-    var productFullInfo = pageTitle + ' ' + pageMetaDescription;
+	var pageMetaOGDescription = '';
 
-    var keywords = extractKeywords(productFullInfo, 2);
+	var pageMetaTwitterDescription = '';
 
-    var searchQuery = keywords.join('+');
+	if (document.querySelector('meta[name="description"]')) {
 
-    var proxyString = 'https://airynok.github.io/?q=' + searchQuery;
+		pageMetaDescription = document
+			.querySelector('meta[name="description"]')
+			.content;
+	}
 
-    window.open(encodeURI(proxyString), '_blank');
+	if (document.querySelector('meta[property="og:description"]')) {
+
+		pageMetaOGDescription = document
+			.querySelector('meta[property="og:description"]')
+			.content;
+	}
+
+	if (document.querySelector('meta[property="twitter:description"]')) {
+
+		pageMetaTwitterDescription = document
+			.querySelector('meta[property="twitter:description"]')
+			.content;
+	}
+
+	var productFullInfo = pageTitle + ' '
+		+ pageMetaDescription + ' '
+		+ pageMetaOGDescription + ' '
+		+ pageMetaTwitterDescription;
+
+	var keywords = extractKeywords(productFullInfo, 2);
+
+	var searchQuery = keywords.join('+');
+
+	var proxyString = 'https://airynok.github.io/?q=' + searchQuery;
+
+	window.open(encodeURI(proxyString), '_blank');
 })();
